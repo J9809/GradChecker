99fa1955029b78613150cadc5d2f24a5b64fa077
J9809 <77200602+J9809@users.noreply.github.com>
2024-03-17 18:23:00 +0900

    page_connection01
    This is a basic setting of page connection.

---
+++ demo/src/main/java/com/gradchecker/demo/LoginController.java
@@ -0,0 +1,19 @@
+
+package com.gradchecker.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+}
