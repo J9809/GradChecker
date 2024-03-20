0fdd4ac01857dd147fd5240e91ea5ba396cf1077
jisong3266 <dbwlthd0726@naver.com>
2024-03-20 15:24:01 +0900

    DB connect and login process
    DB connect and login process

--- demo/src/main/java/com/gradchecker/demo/DemoApplication.java
+++ demo/src/main/java/com/gradchecker/demo/DemoApplication.java
@@ -2,12 +2,24 @@ package com.gradchecker.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoDatabase;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-	}
 
+		// MongoDB에 연결
+		try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+			// 데이터베이스 선택
+			MongoDatabase database = mongoClient.getDatabase("GradChecker");
+
+			System.out.println("Connected to the database successfully.");
+		} catch (Exception e) {
+			System.err.println("An error occurred: " + e);
+		}
+	}
 }
--- demo/src/main/java/com/gradchecker/demo/LoginController.java
+++ demo/src/main/java/com/gradchecker/demo/LoginController.java
@@ -1,11 +1,41 @@
-
 package com.gradchecker.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 
 @Controller
 public class LoginController {
+
+    private final MongoClient mongoClient;
+    private final MongoDatabase database;
+    private final MongoCollection<Document> studentsCollection;
+
+    public LoginController() {
+        // MongoDB에 연결
+        mongoClient = MongoClients.create("mongodb://localhost:27017");
+        database = mongoClient.getDatabase("GradChecker");
+        studentsCollection = database.getCollection("students");
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
+        // MongoDB에서 사용자 정보 확인
+        Document student = studentsCollection.find(new Document("student_id", username)).first();
+        if (student != null && student.getString("pw").equals(password)) {
+            System.out.println("success" + username + password);
+            return "success"; // 인증 성공 시 대시보드 페이지로 리다이렉트
+        } else {
+            System.out.println("fail" + username + password);
+            return "redirect:/login?error"; // 인증 실패 시 로그인 페이지로 리다이렉트 (에러 메시지 포함)
+        }
+    }
 
     @GetMapping("/")
     public String index() {
