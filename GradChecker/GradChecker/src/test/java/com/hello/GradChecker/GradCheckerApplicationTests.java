0afb307669b74a8a5928440b3d173fe9a608e93c
J9809 <77200602+J9809@users.noreply.github.com>
2024-04-15 15:42:13 +0900

    new_gradchecker
    new!

---
+++ GradChecker/GradChecker/src/test/java/com/hello/GradChecker/GradCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hello.GradChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
